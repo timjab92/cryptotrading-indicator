ef63ca786cd7eb4042f67b6ed70222e46f3b1269
Tim Jablonski <timjab92@gmail.com>
2021-08-25 15:06:57 +0200

    updates on the preprocessing

--- cryptotradingindicator/data.py
+++ cryptotradingindicator/data.py
@@ -1,23 +1,63 @@
 from cryptotradingindicator.utils import computeRSI, stoch_rsi, get_bollinger_bands
 import pandas as pd
 import os
+import requests
+import numpy as np
 
 def hello_world():
     return "Hello, crypto trader!!"
 
 
-## GET DATA ##
+## GET DATA_TRAIN ##
 
-def get_csv_data():
+def get_train_data():
     """
     Returns the raw training dataset for the price of bitcoin since 31.12.2011.
     The index is set to the date.
     """
     path= os.path.dirname(os.path.abspath(__file__))
     data = pd.read_csv(os.path.join(path, "../data/BTCUSD_4hours.csv"))
-    data = data.drop(columns="Unnamed: 0").set_index("date")
-    return data
+    data_train = data.drop(columns="Unnamed: 0").set_index("date")
+    return data_train
 
+
+## GET LIVE DATA FROM COINGECKO ##
+
+def get_coingecko():
+    #ohlc
+    url = "https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=30"
+    response = requests.get(url).json()
+    #cleaning
+    data_api = pd.DataFrame(response, columns = ['unix_time','open', 'high', 'low', 'close'])
+    data_api["Date"] = pd.to_datetime(data_api["unix_time"], unit='ms')
+    data_api = data_api.drop(columns='unix_time').set_index('Date')
+
+    #volume
+    url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30"
+    response = requests.get(url).json()
+
+    volume = pd.DataFrame(response['total_volumes'], columns=["unix_time","volume"])
+    volume['date'] = pd.to_datetime(pd.to_datetime(volume['unix_time'],unit='ms').dt.strftime("%Y/%m/%d, %H:00:00"))
+    volume = volume.drop(columns='unix_time').set_index('date')
+
+    #resample hourly into 4h
+    volume = volume.resample("4H").mean()
+
+    #concatinate
+    volume = volume[-180:]
+    data_api = data_api[-181:-1]
+    full = pd.concat([data_api, volume], axis=1)
+    full.columns=['open', 'high', 'low', 'close', 'volume']
+
+    for x in ['open', 'high', 'low', 'close']:
+        full[f'log_{x}'] = full[x].apply(lambda x: np.log(x))
+
+    data_api = full.copy()
+
+    return data_api
+
+
+## PROCESS THE DATA ##
 
 def add_ema(data, tspan=[12,26,20,50,34,55]):
     """
@@ -54,9 +94,11 @@ def add_vol_roc(data):
     return data
 
 
+
+
 if __name__ == '__main__':
-    data= get_csv_data()
-    add_ema(data)
-    add_stoch_rsi(data)
-    add_bollinger(data,data.log_close)
-    add_vol_roc(data)
+    train_data= get_train_data()
+    add_ema(train_data)
+    add_stoch_rsi(train_data)
+    add_bollinger(train_data,train_data.log_close)
+    add_vol_roc(train_data)
--- tests/test_data.py
+++ tests/test_data.py
@@ -7,9 +7,9 @@ def test_length_of_hello_world():
     assert len(hello_world()) != 0
 
 def test_data_shape():
-    data = get_csv_data()
-    add_ema(data)
-    add_stoch_rsi(data)
-    add_bollinger(data,data.log_close)
-    add_vol_roc(data)
-    assert data.shape == (20840, 22)
+    train_data= get_train_data()
+    add_ema(train_data)
+    add_stoch_rsi(train_data)
+    add_bollinger(train_data,train_data.log_close)
+    add_vol_roc(train_data)
+    assert train_data.shape == (20840, 22)
