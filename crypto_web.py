88d12a2c377a54f2d982eb6afcae3581bb419218
Ivan Fernandes <ivanbfernandes@gmail.com>
2021-08-25 10:54:57 +0200

    gcp reset

--- crypto_web.py
+++ crypto_web.py
@@ -63,27 +63,27 @@ fig.update_layout(
                 #   width=1000, height=400,
                   margin=dict(l=40, r=40, b=40, t=40),
                   xaxis=dict(
-        # rangeselector=dict(
-        #     buttons=list([
-        #         dict(count=1,
-        #              label="1m",
-        #              step="month",
-        #              stepmode="backward"),
-        #         dict(count=6,
-        #              label="6m",
-        #              step="month",
-        #              stepmode="backward"),
-        #         dict(count=1,
-        #              label="YTD",
-        #              step="year",
-        #              stepmode="todate"),
-        #         dict(count=1,
-        #              label="1y",
-        #              step="year",
-        #              stepmode="backward"),
-        #         dict(step="all")
-        #     ])
-        # ),
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
         rangeslider=dict(
             visible=True
         ),
--- cryptotradingindicator/data.py
+++ cryptotradingindicator/data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 GCP_PATH = "gs://crypto-indicator/data/BTCUSD_2011-12-31_to_2021-08-23_4hours_Clean.csv"
 LOCAL_PATH = "/home/ivanfernandes/code/timjab92/cryptotradingindicator/data/BTCUSD_2011-12-31_to_2021-08-23_4hours_Clean.csv"
 BUCKET_NAME="crypto-indicator"
@@ -26,6 +25,7 @@ def hello_world():
 
 if __name__ == "__main__":
     params = dict(nrows=1000,
-                  local=False,  # set to False to get data from GCP (Storage or BigQuery)
+                  local=True,  # set to False to get data from GCP (Storage or BigQuery)
                   )
     df = get_gcp_data(**params)
+    print(df.head())
