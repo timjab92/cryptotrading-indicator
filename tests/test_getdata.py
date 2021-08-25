8f60c69205e44c2af6165b4b5a020a0904a115fa
Ivan Fernandes <ivanbfernandes@gmail.com>
2021-08-25 09:33:16 +0200

    remove time package

--- tests/test_getdata.py
+++ tests/test_getdata.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-from cryptotradingindicator.data import get_data
+from cryptotradingindicator.data import get_gcp_data
 
 def test_instance():
-    assert isinstance(get_data(), pd.DataFrame)
+    assert isinstance(get_gcp_data(), pd.DataFrame)
     
 
 def test_header():
-    data = [get_data().columns[i].lower() for i in range(len(list(get_data().columns)))]
+    data = [get_gcp_data().columns[i].lower() for i in range(len(list(get_gcp_data().columns)))]
     assert "open" in data and "close" in data
